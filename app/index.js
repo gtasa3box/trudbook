df46ccd1514b12cb5b95797b97bb2f9c057f052d
gtasa3box <gtasa3box@gmail.com>
2021-05-24 06:15:41 +0300

    final commit

--- app/index.js
+++ app/index.js
@@ -1,5 +1,57 @@
 import './index.scss';
 
+$('.slider').slick({
+
+	dots: false,
+	infinite: false,
+	speed: 300,
+	slidesToShow: 3,
+	slidesToScroll: 1,
+	infinite:true,
+	arrows: true,
+	responsive: [
+		  {
+		breakpoint: 1300,
+		settings: {
+		  slidesToShow: 4,
+		  slidesToScroll: 3,
+		  infinite: true,
+		  dots: true,
+		}
+	  },
+	  {
+		breakpoint: 1024,
+		settings: {
+		  slidesToShow: 3,
+		  slidesToScroll: 3,
+		  infinite: true,
+		  dots: true,
+		}
+	  },
+	  {
+		breakpoint: 820,
+		settings: {
+		  slidesToShow: 2,
+		  slidesToScroll: 2,
+		}
+	  },
+	  {
+		breakpoint: 480,
+		settings: {
+		  slidesToShow: 1,
+		  slidesToScroll: 1,
+		  autoplay: true,
+		  autoplaySpeed: 3000,
+		  arrows: false,
+		}
+	  }
+	]
+});
+
+
+
+
+
 
 
 
