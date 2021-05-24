1c723b10176427baec60063e16dc47117c4167a2
gtasa3box <gtasa3box@gmail.com>
2021-05-25 01:08:08 +0300

    final commit

--- app/index.js
+++ app/index.js
@@ -78,7 +78,34 @@ function toogleclass(click_burger) {
 };
 
 
+'use strict';
+/* begin begin Back to Top button  */
+(function() {
+  function trackScroll() {
+    var scrolled = window.pageYOffset;
+    var coords = document.documentElement.clientHeight;
 
+    if (scrolled > coords) {
+      goTopBtn.classList.add('back_to_top-show');
+    }
+    if (scrolled < coords) {
+      goTopBtn.classList.remove('back_to_top-show');
+    }
+  }
+
+  function backToTop() {
+    if (window.pageYOffset > 0) {
+      window.scrollBy(0, -6000);
+      setTimeout(backToTop, 500); 
+    }
+  }
+
+  var goTopBtn = document.querySelector('.back_to_top');
+
+  window.addEventListener('scroll', trackScroll);
+  goTopBtn.addEventListener('click', backToTop);
+})();
+/* end begin Back to Top button  */
 
 
 
